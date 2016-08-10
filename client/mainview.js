434de8c0f724e119958d3178c91c7e0ce50195c7
llaske <llaske@c2s.fr>
2016-08-10 23:57:28 +0200

    Refactor text handling in a dedicated module

--- client/mainview.js
+++ client/mainview.js
@@ -4,12 +4,12 @@ var
 	platform = require('enyo/platform'),
 	Button = require('moonstone/Button'),
 	Input = require('moonstone/Input'),
-	TextArea = require('moonstone/RichText'),
 	IconButton = require('moonstone/IconButton'),
 	InputDecorator = require('moonstone/InputDecorator'),
 	Popup = require('moonstone/Popup'),
 	Spotlight = require('spotlight'),
 	Dialog = require('./dialog'),
+	SmartTextArea = require('./smarttext'),
 	Ajax = require('enyo/Ajax'),
 	Storage = require('./storage');
 
@@ -24,7 +24,7 @@ module.exports = kind({
 		{name: 'twistButton', kind: IconButton, src: '@./images/twistjs.svg', small: false, spotlight: true, ontap: 'twistButtonTapped'},
 		{name: 'count', content: '0', classes: "twist-count"},
 		{name: 'textDecorator', kind: InputDecorator, spotlight: true, classes: "twist-text-decorator", components: [
-			{name: "text", kind: TextArea, classes: "twist-text", placeholder: 'Enter Twist here', onkeypress: 'keyPressed', onkeydown: 'keyDown', oninput: 'inputChange', onfocus: 'focused'}
+			{name: "text", kind: SmartTextArea, onfocus: 'focused', oninput: 'updateCount', onValidate: 'publishTwist'}
 		]},
 		{name: 'authDialog', kind: Dialog, onHide: 'createTwist'},
 		{name: 'errorPopup', kind: Popup, content: ''}
@@ -43,15 +43,13 @@ module.exports = kind({
 
 		// Initialize
 		Spotlight.initialize(this);
-		this.replaceMode = false;
-		this.replaceValue = "";
+		this.$.text.setTags(["microsoft", "mobile", "android", "dotnet", "facebook", "apple"]);
 	},
 
 	rendered: function() {
 		this.inherited(arguments);
 
 		this.$.url.focus();
-		appendNode(this.$.text, "twist-in-text", "&nbsp;");
 	},
 
 	focused: function(ctrl) {
@@ -67,91 +65,14 @@ module.exports = kind({
 		oldFocus.removeClass('twist-focused');
 	},
 
-	inputChange: function(ctrl, e) {
-		if (!this.updateCount(ctrl)) {
-			appendNode(this.$.text, "twist-in-text", "&nbsp;");
-		}
-	},
-
-	keyDown: function(ctrl, e) {
-		// If TAB press in hashtag mode, replace by current selection
-		if (e.keyCode == 9 && this.replaceMode) {
-			this.$.text.removeSelection();
-			e.preventDefault();
-		}
-	},
-
-	keyPressed: function(ctrl, e){
-		// Ignore BACK
-		if (e.charCode == 0) {
-			return;
-		}
-
-		// ENTER key validate the twist
-		else if (e.charCode == 13) {
-			e.preventDefault();
-			this.publishTwist();
-			return;
-		}
-
-		// In hashtag mode, wait end character
-		var selection = this.$.text.getSelection();
-		var hashtagMode = this.replaceMode || isInHashtag(selection);
-		var char = String.fromCharCode(e.charCode);
-		if (hashtagMode) {
-			if (!char.match(/[a-zA-Z_]/i)) {
-				// Leave hashtag mode
-				if (e.charCode == 32) {
-					char = "&nbsp;"
-				}
-				if (this.replaceMode) {
-					selection.deleteFromDocument();
-				}
-				appendNode(this.$.text, "twist-in-text", char);
-				this.replaceMode = false;
-				this.replaceValue = '';
-				e.preventDefault();
-				return;
-			} else {
-				// Intellisense proposal
-				var tags = ["microsoft", "mobile", "android", "dotnet", "facebook", "apple"];
-				var current;
-				if (this.replaceMode) {
-					current = this.replaceValue+char.toLowerCase();
-				} else {
-				 	current = (getRawSelection(selection)+char).toLowerCase();
-				}
-				for (var i = 0 ; i < tags.length ; i++) {
-console.log("Look for <"+current+"> in <"+tags[i]+">");
-					if (tags[i].indexOf(current) == 0) {
-						var delta = platform.firefox ? -this.replaceValue.length : 1; // HACK: Firefox handle selection differently
-						this.$.text.insertAtCursor(tags[i].substr(current.length-1));
-						selection.extend(selection.anchorNode, current.length+delta);
-						this.replaceMode = true;
-						this.replaceValue = current;
-						e.preventDefault();
-						break;
-					}
-				}
-			}
-		} else {
-			// # key enter in hashtag mode
-			if (e.charCode == 35) {
-				appendNode(this.$.text, "twist-tag-in-text", "#");
-				e.preventDefault();
-				return;
-			}
-		}
-	},
-
 	twistButtonTapped: function() {
 		this.$.urlDecorator.removeClass('twist-focused');
 		this.$.textDecorator.removeClass('twist-focused');
 		this.publishTwist();
 	},
 
-	updateCount: function(ctrl) {
-		var textRaw = getRawtext(ctrl);
+	updateCount: function() {
+		var textRaw = this.$.text.getRawtext();
 		var textLength = textRaw.length;
 		var count = this.$.url.getValue().length+textLength;
 		if (textLength > 1) {
@@ -194,7 +115,7 @@ console.log("Look for <"+current+"> in <"+tags[i]+">");
 
 	apiCallResponse: function(inSender, inResponse) {
 		this.$.url.setValue('');
-		this.$.text.setValue('');
+		this.$.text.setValue(''); // TODO: A reinit is probably better
 	},
 
 	apiCallFail: function(inSender, inError) {
@@ -206,68 +127,3 @@ console.log("Look for <"+current+"> in <"+tags[i]+">");
 		}
 	}
 });
-
-// Private: Convert RichText into a raw text
-function getRawtext(richtext) {
-	var root = richtext.hasNode();
-	if (!root) {
-		return '';
-	}
-	var nodes = root.childNodes;
-	var rawtext = '';
-	for (var i = 0 ; i < nodes.length ; i++) {
-		rawtext += nodes[i].innerText;
-	}
-	return rawtext;
-}
-
-// Private: Detect if the current selection is in hashtag mode
-function isInHashtag(selection) {
-	if (!selection || !selection.anchorNode || !selection.anchorNode.parentNode) {
-		return false;
-	}
-	if (selection.anchorNode.className == "twist-tag-in-text") {
-		return true;
-	} else if (selection.anchorNode.parentNode.className == "twist-tag-in-text") {
-		return true;
-	}
-	return false;
-}
-
-// Private: Get current selection as raw text
-function getRawSelection(selection) {
-	if (!selection || !selection.anchorNode || !selection.anchorNode.parentNode) {
-		return "";
-	}
-	if (selection.anchorNode.className == "twist-tag-in-text") {
-		return "";
-	} else if (selection.anchorNode.parentNode.className == "twist-tag-in-text") {
-		var text = '';
-		var nodes = selection.anchorNode.parentNode.childNodes;
-		for (var i = 0 ; i < nodes.length ; i++) {
-			text += nodes[i].nodeValue;
-		}
-		return text.substr(1);
-	}
-	return "";
-}
-
-// Private: Create a new node into the current selection
-function appendNode(ctrl, className, text) {
-	var root = ctrl.hasNode();
-	if (!root) {
-		return;
-	}
-	var container = document.createElement("div");
-	container.className = className;
-	container.innerHTML = text;
-	root.appendChild(container);
-	var range = document.createRange();
-	var selection = ctrl.getSelection();
-	if (selection) {
-		range.selectNode(container);
-		selection.removeAllRanges();
-		selection.addRange(range);
-		selection.collapse(container, 1);
-	}
-}
---
+++ client/smarttext.js
@@ -0,0 +1,193 @@
+var
+	util = require('enyo/utils'),
+	kind = require('enyo/kind'),
+	platform = require('enyo/platform'),
+	TextArea = require('moonstone/RichText');
+
+module.exports = kind({
+	name: "smarttextarea",
+	kind: TextArea,
+	classes: "twist-text",
+	handlers: {
+		onkeypress: 'keyPressed',
+		onkeydown: 'keyDown',
+		oninput: 'inputChange'
+	},
+	events: {
+		onValidate: ''
+	},
+	published: {
+		tags: []
+	},
+
+	create: function() {
+		this.inherited(arguments);
+
+		this.replaceMode = false;
+		this.replaceValue = "";
+	},
+
+	rendered: function() {
+		this.inherited(arguments);
+
+		appendNode(this, "twist-in-text", "&nbsp;");
+	},
+
+	inputChange: function(ctrl, e) {
+		var newSize = this.getRawtext().length;
+		if (!newSize) {
+			// HACK: Remove all dummy text (BR, ...) when text is empty
+			var root = this.hasNode();
+			if (!root) {
+				return;
+			}
+			while (root.hasChildNodes()) {
+				root.removeChild(root.firstChild);
+			}
+			appendNode(this, "twist-in-text", "&nbsp;");
+		}
+	},
+
+	keyDown: function(ctrl, e) {
+		// If TAB press in hashtag mode, replace by current selection
+		if (e.keyCode == 9 && this.replaceMode) {
+			this.removeSelection();
+			e.preventDefault();
+		}
+
+		// ENTER key validate the twist
+		else if (e.keyCode == 13) {
+			this.doValidate();
+			e.preventDefault();
+			return;
+		}
+	},
+
+	keyPressed: function(ctrl, e){
+		// Ignore BACK
+		if (e.charCode == 0) {
+			return;
+		}
+
+		// ENTER key validate the twist
+		else if (e.charCode == 13) {
+			this.doValidate();
+			e.preventDefault();
+			return;
+		}
+
+		// In hashtag mode, wait end character
+		var selection = this.getSelection();
+		var hashtagMode = this.replaceMode || isInHashtag(selection);
+		var char = String.fromCharCode(e.charCode);
+		if (hashtagMode) {
+			if (!char.match(/[a-zA-Z_]/i)) {
+				// Leave hashtag mode
+				if (e.charCode == 32) {
+					char = "&nbsp;"
+				}
+				if (this.replaceMode) {
+					selection.deleteFromDocument();
+				}
+				appendNode(this, "twist-in-text", char);
+				this.replaceMode = false;
+				this.replaceValue = '';
+				e.preventDefault();
+				return;
+			} else {
+				// Intellisense proposal
+				var tags = this.tags;
+				var current;
+				if (this.replaceMode) {
+					current = this.replaceValue+char.toLowerCase();
+				} else {
+				 	current = (getRawSelection(selection)+char).toLowerCase();
+				}
+				for (var i = 0 ; i < tags.length ; i++) {
+console.log("Look for <"+current+"> in <"+tags[i]+">");
+					if (tags[i].indexOf(current) == 0) {
+						var delta = platform.firefox ? -this.replaceValue.length : 1; // HACK: Firefox handle selection differently
+						this.insertAtCursor(tags[i].substr(current.length-1));
+						selection.extend(selection.anchorNode, current.length+delta);
+						this.replaceMode = true;
+						this.replaceValue = current;
+						e.preventDefault();
+						break;
+					}
+				}
+			}
+		} else {
+			// # key enter in hashtag mode
+			if (e.charCode == 35) {
+				appendNode(this, "twist-tag-in-text", "#");
+				e.preventDefault();
+				return;
+			}
+		}
+	},
+
+	getRawtext: function() {
+		var root = this.hasNode();
+		if (!root) {
+			return '';
+		}
+		var nodes = root.childNodes;
+		var rawtext = '';
+		for (var i = 0 ; i < nodes.length ; i++) {
+			rawtext += nodes[i].innerText;
+		}
+		return rawtext;
+	}
+});
+
+
+// Private: Detect if the current selection is in hashtag mode
+function isInHashtag(selection) {
+	if (!selection || !selection.anchorNode || !selection.anchorNode.parentNode) {
+		return false;
+	}
+	if (selection.anchorNode.className == "twist-tag-in-text") {
+		return true;
+	} else if (selection.anchorNode.parentNode.className == "twist-tag-in-text") {
+		return true;
+	}
+	return false;
+}
+
+// Private: Get current selection as raw text
+function getRawSelection(selection) {
+	if (!selection || !selection.anchorNode || !selection.anchorNode.parentNode) {
+		return "";
+	}
+	if (selection.anchorNode.className == "twist-tag-in-text") {
+		return "";
+	} else if (selection.anchorNode.parentNode.className == "twist-tag-in-text") {
+		var text = '';
+		var nodes = selection.anchorNode.parentNode.childNodes;
+		for (var i = 0 ; i < nodes.length ; i++) {
+			text += nodes[i].nodeValue;
+		}
+		return text.substr(1);
+	}
+	return "";
+}
+
+// Private: Create a new node into the current selection
+function appendNode(ctrl, className, text) {
+	var root = ctrl.hasNode();
+	if (!root) {
+		return;
+	}
+	var container = document.createElement("div");
+	container.className = className;
+	container.innerHTML = text;
+	root.appendChild(container);
+	var range = document.createRange();
+	var selection = ctrl.getSelection();
+	if (selection) {
+		range.selectNode(container);
+		selection.removeAllRanges();
+		selection.addRange(range);
+		selection.collapse(container, 1);
+	}
+}
