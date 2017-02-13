4a973a86f261d5ea86de3016ba00650d2e5a4eea
llaske <llaske@c2s.fr>
2017-02-13 21:37:56 +0100

    Refactor SmartText to allow tag everywhere

--- client/smarttext.js
+++ client/smarttext.js
@@ -25,13 +25,11 @@ module.exports = kind({
 		this.inherited(arguments);
 
 		this.replaceMode = false;
-		this.replaceValue = "";
+		this.allowHtml = true;
 	},
 
 	rendered: function() {
 		this.inherited(arguments);
-
-		appendNode(this, "twist-in-text", "&nbsp;");
 	},
 
 	inputChange: function(ctrl, e) {
@@ -73,67 +71,123 @@ module.exports = kind({
 			return;
 		}
 
-		// In hashtag mode, wait end character
-		var selection = this.getSelection();
-		var hashtagMode = this.replaceMode || isInHashtag(selection);
+		// Get char
 		var char = String.fromCharCode(e.charCode);
+
+		// Compute selection
+		var currentValue = unformatValue(this.getValue());
+		var selection = this.getRawSelection();
+
+		// Hashtag mode handling
+		var hashtagSelection = isInHashtag(currentValue, selection);
+		var hashtagMode = hashtagSelection;
 		if (hashtagMode) {
-			if (!char.match(/[a-zA-Z_]/i)) {
-				// Leave hashtag mode
-				if (e.charCode == 32) {
-					char = "&nbsp;"
-				}
-				if (this.replaceMode) {
-					selection.deleteFromDocument();
-				}
-				appendNode(this, "twist-in-text", char);
-				this.replaceMode = false;
-				this.replaceValue = '';
-				e.preventDefault();
-				return;
-			} else {
-				// Intellisense proposal
-				var tags = this.tags;
-				var current;
-				if (this.replaceMode) {
-					current = this.replaceValue+char.toLowerCase();
-				} else {
-				 	current = (getRawSelection(selection)+char).toLowerCase();
-				}
-				for (var i = 0 ; i < tags.length ; i++) {
-					if (tags[i].indexOf(current) == 0) {
-						var delta = platform.firefox ? -this.replaceValue.length : 1; // HACK: Firefox handle selection differently
-						this.insertAtCursor(tags[i].substr(current.length-1));
-						selection.extend(selection.anchorNode, current.length+delta);
-						this.replaceMode = true;
-						this.replaceValue = current;
-						e.preventDefault();
-						break;
-					}
+			// Cut string before and after the current tag
+			var before = currentValue.substr(0, hashtagSelection.start);
+			var after = currentValue.substr(hashtagSelection.end+1);
+			var tag = currentValue.substr(hashtagSelection.start+1, hashtagSelection.end-hashtagSelection.start);
+			var beforeCursorInTag = tag.substr(0, selection.start-hashtagSelection.start-1);
+			var afterCursorInTag = tag.substr(selection.start-hashtagSelection.start-1);
+			var newSelection = {start: selection.start+1, end: selection.start+1};
+			var beforeAndTag = before+"#";
+			var newValue = beforeAndTag+beforeCursorInTag+char+afterCursorInTag+after;
+
+			// Intellisense proposal
+			var tags = this.tags;
+			var toFind = beforeCursorInTag+char;
+			var found = false;
+			for (var i = 0 ; i < tags.length ; i++) {
+				if (tags[i].indexOf(toFind) == 0) {
+					newValue = beforeAndTag+tags[i]+after;
+					newSelection.start = beforeAndTag.length+beforeCursorInTag.length+1;
+					newSelection.end = beforeAndTag.length+tags[i].length;
+					this.replaceMode = true;
+					found = true;
+					break;
 				}
 			}
+
+			// New tag
+			if (!found && this.replaceMode) {
+				newValue = beforeAndTag+beforeCursorInTag+char+after;
+			}
+
+			// Replace text
+			this.setValue(formatValue(newValue));
+			this.setRawSelection(newSelection.start, newSelection.end);
 		} else {
-			// # key enter in hashtag mode
-			if (e.charCode == 35) {
-				appendNode(this, "twist-tag-in-text", "#");
-				e.preventDefault();
-				return;
+			// Set new value
+			var newValue;
+			if (selection.start == currentValue.length) {
+				newValue = currentValue + char;
+			} else {
+				newValue = currentValue.substr(0, selection.start)+char+currentValue.substr(selection.end);
 			}
+			this.setValue(formatValue(newValue));
+			this.setRawSelection(selection.start+1, selection.start+1);
+			this.replaceMode = false;
 		}
+
+		e.preventDefault();
 	},
 
 	getRawtext: function() {
+		return unformatValue(this.getValue());
+	},
+
+	getRawSelection: function() {
 		var root = this.hasNode();
 		if (!root) {
-			return '';
+			return {start:-1, end: -1};
 		}
+		var selection = this.getSelection();
+		var anchorNode = selection.anchorNode;
+		var focusNode = selection.focusNode;
 		var nodes = root.childNodes;
-		var rawtext = '';
+		var start = 0;
+		var end = 0;
+		var current = 0;
 		for (var i = 0 ; i < nodes.length ; i++) {
-			rawtext += nodes[i].innerText;
+			var node = nodes[i];
+			var child = node.childNodes.length >= 1 ? node.childNodes[0] : null;
+			if (node == anchorNode || child == anchorNode) {
+				start = current + selection.anchorOffset;
+			}
+			if (node == focusNode || child == focusNode) {
+				end = current + selection.focusOffset;
+			}
+			current += node.innerText ? node.innerText.length : node.length;
 		}
-		rawtext = rawtext.replace('\n', ' ').trim();
-		return rawtext;
+		return {start: start, end: end};
+	},
+
+	setRawSelection: function(start, end) {
+		var root = this.hasNode();
+		if (!root) {
+			return;
+		}
+		var selection = this.getSelection();
+		var anchorNode;
+		var focusNode;
+		var nodes = root.childNodes;
+		var range = document.createRange();
+		var current = 0;
+		for (var i = 0 ; i < nodes.length ; i++) {
+			var node = nodes[i];
+			if (node.childNodes.length >= 1) {
+				node = node.childNodes[0];
+			}
+			var length = node.innerText ? node.innerText.length : node.length;
+			if (start >= current && start <= current+length) {
+				range.setStart(node, start-current);
+			}
+			if (end >= current && end <= current+length) {
+				range.setEnd(node, end-current);
+			}
+			current += length;
+		}
+		selection.removeAllRanges();
+		selection.addRange(range);
 	},
 
 	reset: function() {
@@ -144,58 +198,50 @@ module.exports = kind({
 		while (root.firstChild) {
 			root.removeChild(root.firstChild);
 		}
-		appendNode(this, "twist-in-text", "&nbsp;");
 	}
 });
 
 
 // Private: Detect if the current selection is in hashtag mode
-function isInHashtag(selection) {
-	if (!selection || !selection.anchorNode || !selection.anchorNode.parentNode) {
-		return false;
-	}
-	if (selection.anchorNode.className == "twist-tag-in-text") {
-		return true;
-	} else if (selection.anchorNode.parentNode.className == "twist-tag-in-text") {
-		return true;
-	}
-	return false;
-}
-
-// Private: Get current selection as raw text
-function getRawSelection(selection) {
-	if (!selection || !selection.anchorNode || !selection.anchorNode.parentNode) {
-		return "";
-	}
-	if (selection.anchorNode.className == "twist-tag-in-text") {
-		return "";
-	} else if (selection.anchorNode.parentNode.className == "twist-tag-in-text") {
-		var text = '';
-		var nodes = selection.anchorNode.parentNode.childNodes;
-		for (var i = 0 ; i < nodes.length ; i++) {
-			text += nodes[i].nodeValue;
+function isInHashtag(value, selection) {
+	var start = -1;
+	for (var i = selection.start-1 ; i >= 0 && i < value.length ; i--) {
+		var char = value[i];
+		if (char == '#') {
+			start = i;
+			break;
+		} else if (!char.match(/[a-zA-Z_]/i)) {
+			return null;
 		}
-		return text.substr(1);
 	}
-	return "";
+
+	var end = -1;
+	for (var i = start+1 ; i < value.length ; i++) {
+		var char = value[i];
+		if (!char.match(/[a-zA-Z_]/i)) {
+			break;
+		}
+		end = i;
+	}
+
+	if (start == -1) {
+		return null;
+	}
+
+	return {start: start, end: (end==-1?start:end)};
 }
 
-// Private: Create a new node into the current selection
-function appendNode(ctrl, className, text) {
-	var root = ctrl.hasNode();
-	if (!root) {
-		return;
-	}
-	var container = document.createElement("div");
-	container.className = className;
-	container.innerHTML = text;
-	root.appendChild(container);
-	var range = document.createRange();
-	var selection = ctrl.getSelection();
-	if (selection) {
-		range.selectNode(container);
-		selection.removeAllRanges();
-		selection.addRange(range);
-		selection.collapse(container, 1);
-	}
+// Private: Format/unformat field value
+function formatValue(value) {
+	var formated = value.replace(/#[a-zA-Z_]+/gi,
+		function formatTag(tag) {
+			return "<div class='twist-tag-in-text'>"+tag+"</div>";
+	});
+	return formated;
+}
+function unformatValue(value) {
+	var unformated = value.replace(/<div class="twist-tag-in-text">/gi, "");
+	unformated = unformated.replace(/<div class='twist-tag-in-text'>/gi, "")
+	unformated = unformated.replace(/<\/div>/gi, "");
+	return unformated;
 }
